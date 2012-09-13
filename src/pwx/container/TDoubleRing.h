3c2c1a93ebc5b9a8a49f06a4b438b7a7ad16af75
Sven Eden <sven.eden@gmx.de>
2012-09-13 19:53:03 +0200

    Added bare files for TSingleRing, TDoubleRing and TSet to be filled, next.

---
+++ src/pwx/container/TDoubleRing.h
@@ -0,0 +1,117 @@
+#pragma once
+#ifndef PWX_PWXLIB_PWX_CONTAINER_TDOUBLERING_H_INCLUDED
+#define PWX_PWXLIB_PWX_CONTAINER_TDOUBLERING_H_INCLUDED 1
+
+/** @file <filename>
+  *
+  * @brief <description>
+  *
+  * (c) 2007 - 2012 PrydeWorX
+  * @author Sven Eden, PrydeWorX - Bardowick, Germany
+  *         yamakuzure@users.sourceforge.net
+  *         http://pwxlib.sourceforge.net
+  *
+  *  This program is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  *
+  * History and Changelog are maintained in pwx.h
+**/
+
+#include <pwx/container/TDoubleList.h>
+
+namespace pwx {
+
+/** @class TDoubleRing
+  *
+  * @brief
+  *
+  * @todo : Describe properly
+**/
+template<typename data_t>
+class TDoubleRing : TDoubleList<data_t>
+{
+public:
+  /* ===============================================
+   * === Public types                            ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public Constructors and destructors     ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public methods                          ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public operators                        ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public members                          ===
+   * ===============================================
+  */
+
+protected:
+  /* ===============================================
+   * === Protected Constructors and destructors  ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Protected methods                       ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Protected operators                     ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Protected members                       ===
+   * ===============================================
+  */
+
+private:
+  /* ===============================================
+   * === Private Constructors and destructors    ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Private methods                         ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Private operators                       ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Private members                         ===
+   * ===============================================
+  */
+
+}; // class TDoubleRing
+
+
+} // namespace pwx
+#endif // PWX_PWXLIB_PWX_CONTAINER_TDOUBLERING_H_INCLUDED
+
---
+++ src/pwx/container/TSet.h
@@ -0,0 +1,118 @@
+#pragma once
+#ifndef PWX_PWXLIB_PWX_CONTAINER_TSET_H_INCLUDED
+#define PWX_PWXLIB_PWX_CONTAINER_TSET_H_INCLUDED 1
+
+/** @file <filename>
+  *
+  * @brief <description>
+  *
+  * (c) 2007 - 2012 PrydeWorX
+  * @author Sven Eden, PrydeWorX - Bardowick, Germany
+  *         yamakuzure@users.sourceforge.net
+  *         http://pwxlib.sourceforge.net
+  *
+  *  This program is free software: you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation, either version 3 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful,
+  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *  GNU General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  * History and Changelog are maintained in pwx.h
+**/
+
+#include <pwx/types/CLockable.h>
+#include <pwx/general/macros.h>
+#include <pwx/container/TDoubleList.h>
+
+namespace pwx {
+
+/** @class TSet
+  *
+  * @brief
+  *
+  * @todo : Describe properly
+**/
+template<typename T>
+class TSet
+{
+public:
+  /* ===============================================
+   * === Public types                            ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public Constructors and destructors     ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public methods                          ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public operators                        ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Public members                          ===
+   * ===============================================
+  */
+
+protected:
+  /* ===============================================
+   * === Protected Constructors and destructors  ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Protected methods                       ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Protected operators                     ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Protected members                       ===
+   * ===============================================
+  */
+
+private:
+  /* ===============================================
+   * === Private Constructors and destructors    ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Private methods                         ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Private operators                       ===
+   * ===============================================
+  */
+
+  /* ===============================================
+   * === Private members                         ===
+   * ===============================================
+  */
+
+}; // class TSet
+
+
+} // namespace pwx
+#endif // PWX_PWXLIB_PWX_CONTAINER_TSET_H_INCLUDED
+
