e4f8a0a8a6409950ad6a0f768b459229ba6cad81
Sven Eden <sven.eden@gmx.de>
2012-10-12 07:43:05 +0200

    Updated TSingle/DoubleElement to new layout, added some documentation and a new method destroyed() that returns true if the data is destroyed:

--- src/pwx/types/TDoubleElement.h
+++ src/pwx/types/TDoubleElement.h
@@ -31,7 +31,8 @@
 #include <pwx/types/CLockable.h>
 #include <pwx/types/TVarDeleter.h>
 
-namespace pwx {
+namespace pwx
+{
 
 /** @struct TDoubleElement
   *
@@ -44,6 +45,15 @@ namespace pwx {
   * to destroy the data when the element is deleted. If no such function was set,
   * the standard delete operator is used instead.
   *
+  * The data pointer itself is wrapped into an std::shared_ptr. It is therefore
+  * completely safe to copy TsingleElement instances.
+  *
+  * The data pointer itself is public. You can use foo->data.get() to access it.
+  * Further the operator* is overloaded and **foo will result in a reference to the
+  * data.
+  *
+  * The next element in the list can be retrieved using the public foo->next pointer.
+  *
   * It is recommended that you use the much more advanced std::list unless you
   * need to store a very large number of elements and can not live with the
   * downside of every element having to be copied into the std::list.
@@ -53,117 +63,160 @@ namespace pwx {
 template<typename data_t>
 struct PWX_API TDoubleElement : public CLockable
 {
-  /* ===============================================
-   * === Public types                            ===
-   * ===============================================
-  */
-  typedef CLockable                 base_t;
-  typedef TDoubleElement<data_t>    elem_t;
-  typedef ::std::shared_ptr<data_t> share_t;
 
-  /* ===============================================
-   * === Public Constructors and destructors     ===
-   * ===============================================
-  */
+	/* ===============================================
+	 * === Public types                            ===
+	 * ===============================================
+	*/
+	typedef CLockable                 base_t;
+	typedef TDoubleElement<data_t>    elem_t;
+	typedef ::std::shared_ptr<data_t> share_t;
 
-  /** @brief default constructor
-    *
-    * The default constructor sets both the data pointer and the destroy method.
-    *
-    * @param data_[in] A pointer to the data this list element is to hold.
-    * @param destroy_[in] A pointer to a function that is to be used to destroy the data
-  **/
-  TDoubleElement(data_t* data_, void (*destroy_)(data_t* data_)) noexcept
-  : data(data_, TVarDeleter<data_t>(destroy_))
-  { }
 
-  /** @brief explicit constructor
-    *
-    * Delegating ctor that calls the default ctor with destroy_ being the nullptr
-    *
-    * @param data_[in] A pointer to the data this list element is to hold.
-  **/
-  explicit
-  TDoubleElement(data_t* data_) noexcept
-  : elem_t(data_, nullptr)
-    { }
+	/* ===============================================
+	 * === Public Constructors and destructors     ===
+	 * ===============================================
+	*/
 
-  TDoubleElement() PWX_DELETE;
+	/** @brief default constructor
+	  *
+	  * The default constructor sets both the data pointer and the destroy method.
+	  *
+	  * @param[in] data_ A pointer to the data this list element is to hold.
+	  * @param[in] destroy_ A pointer to a function that is to be used to destroy the data
+	**/
+	TDoubleElement (data_t* data_, void (*destroy_) (data_t* data_)) noexcept
+	: data (data_, TVarDeleter<data_t> (destroy_))
+	{ }
 
-  /** @brief copy ctor
-    *
-    * The copy ctor creates a stand-alone element without neighbors
-    * copying the data pointer and destroy method from @a src.
-    * Further it will notice all elements holding the same pointer
-    * of its presence.
-    * As the data is wrapped in a shared_ptr, data will not get
-    * deleted unless the last reference is gone.
-    *
-    * @param[in] src reference to the element to copy.
-  **/
-  TDoubleElement(const elem_t &src) noexcept
-  : base_t(src),
-    data(src.data)
-    { }
 
-  virtual ~TDoubleElement() noexcept;
+	/** @brief explicit constructor
+	  *
+	  * Delegating ctor that calls the default ctor with destroy_ being the nullptr
+	  *
+	  * @param[in] data_ A pointer to the data this list element is to hold.
+	**/
+	explicit
+	TDoubleElement (data_t* data_) noexcept
+	: elem_t (data_, nullptr)
+	{ }
 
-  /* ===============================================
-   * === Public methods                          ===
-   * ===============================================
-  */
 
-  /* ===============================================
-   * === Public operators                        ===
-   * ===============================================
-  */
-  /** @brief assignment operator
-    *
-    * The assignment operator copies over the element and
-    * the destroy method. This element will stay where it
-    * is, and not change its position.
-  **/
-  elem_t& operator=(const elem_t &src) noexcept
-    {
-      PWX_LOCK_GUARD(elem_t, this)
-      data = src.data;
-    }
+	TDoubleElement() PWX_DELETE; // nullptr data is not allowed!
 
-  /** @brief dereferencing an element returns a reference to the stored data
-    *
-    * If the data pointer is nullptr, a pwx::CException with the name
-    * "NullDataException" is thrown.
-    *
-    * @return a read/write reference to the stored data.
-  **/
-  data_t &operator*()
-    {
-      if (nullptr == data.get())
-        PWX_THROW("NullDataException", "nullptr TDoubleElement<T>->data", "The data pointer to dereference is nullptr.")
-      return *data;
-    }
 
-  /** @brief dereferencing a constant element returns a constant reference to the stored data
-    *
-    * If the data pointer is nullptr, a pwx::CException with the name
-    * "NullDataException" is thrown.
-    *
-    * @return a read only reference to the stored data.
-  **/
-  const data_t &operator*() const
-    {
-      if (nullptr == data.get())
-        PWX_THROW("NullDataException", "nullptr TDoubleElement<T>->data", "The data pointer to dereference is nullptr.")
-      return *data;
-    }
+	/** @brief copy ctor
+	  *
+	  * The copy ctor creates a stand-alone element without neighbors
+	  * copying the data pointer and destroy method from @a src.
+	  * Further it will notice all elements holding the same pointer
+	  * of its presence.
+	  * As the data is wrapped in a shared_ptr, data will not get
+	  * deleted unless the last reference is gone.
+	  *
+	  * @param[in] src reference to the element to copy.
+	**/
+	TDoubleElement (const elem_t &src) noexcept
+	: base_t (src),
+	  data (src.data)
+	{ }
 
-  /* ===============================================
-   * === Public members                          ===
-   * ===============================================
-  */
-  share_t data;           //!< The data this list element points to, wrapped in a shared_ptr.
-  elem_t* next = nullptr; //!< The next element in the list or nullptr if this is the tail
-  elem_t* prev = nullptr; //!< The previous element in the list or nullptr if this is the head
+
+	virtual ~TDoubleElement() noexcept;
+
+
+	/* ===============================================
+	 * === Public methods                          ===
+	 * ===============================================
+	*/
+
+	/** @brief returns true if the data  was destroyed
+	  *
+	  * The destructor will try to get a final lock on the
+	  * element when it is destroyed. If another thread
+	  * acquires a lock between the data destruction and
+	  * this final dtor lock, destroy() will return "true".
+	  *
+	  * @return true if the element is within its destruction process.
+	**/
+	bool destroyed() const noexcept { return isDestroyed; }
+
+
+	/* ===============================================
+	 * === Public operators                        ===
+	 * ===============================================
+	*/
+
+	/** @brief assignment operator
+	  *
+	  * The assignment operator copies over the element and
+	  * the destroy method. This element will stay where it
+	  * is, and not change its position.
+	  *
+	  * @param[in] src const reference of the element to copy
+	  * @return reference to this element
+	**/
+	elem_t& operator= (const elem_t &src) noexcept
+	{
+		if (this != &src) {
+			PWX_LOCK_GUARD (elem_t, this)
+			data	= src.data;
+		}
+	}
+
+
+	/** @brief dereferencing an element returns a reference to the stored data
+	  *
+	  * If the data pointer is nullptr, a pwx::CException with the name
+	  * "NullDataException" is thrown.
+	  *
+	  * @return a read/write reference to the stored data.
+	**/
+	data_t &operator*()
+	{
+		if (nullptr == data.get())
+			PWX_THROW ( "NullDataException",
+						"nullptr TDoubleElement<T>->data",
+						"The data pointer to dereference is nullptr.")
+		return *data;
+	}
+
+
+	/** @brief dereferencing a constant element returns a constant reference to the stored data
+	  *
+	  * If the data pointer is nullptr, a pwx::CException with the name
+	  * "NullDataException" is thrown.
+	  *
+	  * @return a read only reference to the stored data.
+	**/
+	const data_t &operator*() const
+	{
+		if (nullptr == data.get())
+			PWX_THROW ( "NullDataException",
+						"nullptr TDoubleElement<T>->data",
+						"The data pointer to dereference is nullptr.")
+		return *data;
+	}
+
+
+	/* ===============================================
+	 * === Public members                          ===
+	 * ===============================================
+	*/
+
+	share_t data;           //!< The data this list element points to, wrapped in a shared_ptr.
+	elem_t* next = nullptr; //!< The next element in the list or nullptr if this is the tail
+	elem_t* prev = nullptr; //!< The previous element in the list or nullptr if this is the head
+
+
+private:
+
+	/* ===============================================
+	 * === Private members                         ===
+	 * ===============================================
+	*/
+
+	bool isDestroyed = false; //!< Set to false by the dtor to let other threads react if they ask destroyed().
 }; // struct TDoubleElement
 
 
@@ -178,18 +231,18 @@ struct PWX_API TDoubleElement : public CLockable
 **/
 template<typename data_t>
 TDoubleElement<data_t>::~TDoubleElement() noexcept
-  {
-    if (1 == data.use_count())
-      {
-        this->lock();
-        data.reset(); // the shared_ptr will delete the data now
-        this->unlock();
+{
+	isDestroyed = true;
 
-        // Do another locking, so that threads having had to wait while the data
-        // was destroyed have a chance now to react before the object is gone
-        PWX_LOCK_GUARD(elem_t, this)
-      }
-  }
+	if (1 == data.use_count()) {
+		PWX_LOCK_GUARD (elem_t, this)
+		PWX_TRY(data.reset()) // the shared_ptr will delete the data now
+		catch(...) { }
+	}
+	// Do another locking, so that threads having had to wait while the data
+	// was destroyed have a chance now to react before the object is gone.
+	PWX_LOCK_GUARD (elem_t, this)
+}
 
 
 } // namespace pwx
--- src/pwx/types/TSingleElement.h
+++ src/pwx/types/TSingleElement.h
@@ -31,7 +31,8 @@
 #include <pwx/types/CLockable.h>
 #include <pwx/types/TVarDeleter.h>
 
-namespace pwx {
+namespace pwx
+{
 
 /** @struct TSingleElement
   *
@@ -44,6 +45,15 @@ namespace pwx {
   * to destroy the data when the element is deleted. If no such function was set,
   * the standard delete operator is used instead.
   *
+  * The data pointer itself is wrapped into an std::shared_ptr. It is therefore
+  * completely safe to copy TsingleElement instances.
+  *
+  * The data pointer itself is public. You can use foo->data.get() to access it.
+  * Further the operator* is overloaded and **foo will result in a reference to the
+  * data.
+  *
+  * The next element in the list can be retrieved using the public foo->next pointer.
+  *
   * It is recommended that you use the much more advanced std::list unless you
   * need to store a very large number of elements and can not live with the
   * downside of every element having to be copied into the std::list.
@@ -53,118 +63,159 @@ namespace pwx {
 template<typename data_t>
 struct PWX_API TSingleElement : public CLockable
 {
-  /* ===============================================
-   * === Public types                            ===
-   * ===============================================
-  */
-  typedef CLockable                 base_t;
-  typedef TSingleElement<data_t>    elem_t;
-  typedef ::std::shared_ptr<data_t> share_t;
 
-  /* ===============================================
-   * === Public Constructors and destructors     ===
-   * ===============================================
-  */
+	/* ===============================================
+	 * === Public types                            ===
+	 * ===============================================
+	*/
 
-  /** @brief default constructor
-    *
-    * The default constructor sets both the data pointer and the destroy method.
-    *
-    * @param data_[in] A pointer to the data this list element is to hold.
-    * @param destroy_[in] A pointer to a function that is to be used to destroy the data
-  **/
-  TSingleElement(data_t* data_, void (*destroy_)(data_t* data_)) noexcept
-  : data(data_, TVarDeleter<data_t>(destroy_))
-    { }
-
-  /** @brief explicit constructor
-    *
-    * Delegating ctor that calls the default ctor with destroy_ being the nullptr
-    *
-    * @param data_[in] A pointer to the data this list element is to hold.
-  **/
-  explicit
-  TSingleElement(data_t* data_) noexcept
-  : elem_t(data_, nullptr)
-    { }
-
-  TSingleElement() PWX_DELETE;
-
-  virtual ~TSingleElement() noexcept;
+	typedef CLockable                 base_t;
+	typedef TSingleElement<data_t>    elem_t;
+	typedef ::std::shared_ptr<data_t> share_t;
 
 
-  /** @brief copy ctor
-    *
-    * The copy ctor creates a stand-alone element without neighbors
-    * copying the data pointer and destroy method from @a src.
-    * Further it will notice all elements holding the same pointer
-    * of its presence.
-    * As the data is wrapped in a shared_ptr, data will not get
-    * deleted unless the last reference is gone.
-    *
-    * @param[in] src reference to the element to copy.
-  **/
-  TSingleElement(const elem_t &src) noexcept
-  : base_t(src),
-    data(src.data)
-    { }
+	/* ===============================================
+	 * === Public Constructors and destructors     ===
+	 * ===============================================
+	*/
 
-  /* ===============================================
-   * === Public methods                          ===
-   * ===============================================
-  */
+	/** @brief default constructor
+	  *
+	  * The default constructor sets both the data pointer and the destroy method.
+	  *
+	  * @param[in] data_ A pointer to the data this list element is to hold.
+	  * @param[in] destroy_ A pointer to a function that is to be used to destroy the data
+	**/
+	TSingleElement (data_t* data_, void (*destroy_) (data_t* data_)) noexcept
+	: data (data_, TVarDeleter<data_t> (destroy_))
+	{ }
 
-  /* ===============================================
-   * === Public operators                        ===
-   * ===============================================
-  */
 
-  /** @brief assignment operator
-    *
-    * The assignment operator copies over the element and
-    * the destroy method. This element will stay where it
-    * is, and not change its position.
-  **/
-  elem_t& operator=(const elem_t &src) noexcept
-    {
-      PWX_LOCK_GUARD(elem_t, this)
-      data = src.data;
-    }
+	/** @brief explicit constructor
+	  *
+	  * Delegating ctor that calls the default ctor with destroy_ being the nullptr
+	  *
+	  * @param[in] data_ A pointer to the data this list element is to hold.
+	**/
+	explicit
+	TSingleElement (data_t* data_) noexcept
+	: elem_t (data_, nullptr)
+	{ }
 
-  /** @brief dereferencing an element returns a reference to the stored data
-    *
-    * If the data pointer is nullptr, a pwx::CException with the name
-    * "NullDataException" is thrown.
-    *
-    * @return a read/write reference to the stored data.
-  **/
-  data_t &operator*()
-    {
-      if (nullptr == data.get())
-        PWX_THROW("NullDataException", "nullptr TSingleElement<T>->data", "The pointer lhs->data to dereference is nullptr.")
-      return *data;
-    }
 
-  /** @brief dereferencing a constant element returns a constant reference to the stored data
-    *
-    * If the data pointer is nullptr, a pwx::CException with the name
-    * "NullDataException" is thrown.
-    *
-    * @return a read only reference to the stored data.
-  **/
-  const data_t &operator*() const
-    {
-      if (nullptr == data.get())
-        PWX_THROW("NullDataException", "nullptr TSingleElement<T>->data", "The pointer lhs->data to dereference is nullptr.")
-      return *data;
-    }
+	TSingleElement() PWX_DELETE; // nullptr data is not allowed
 
-  /* ===============================================
-   * === Public members                          ===
-   * ===============================================
-  */
-  share_t data;           //!< The data this list element points to, wrapped in a shared_ptr.
-  elem_t* next = nullptr; //!< The next element in the list or nullptr if this is the tail.
+
+	/** @brief copy ctor
+	  *
+	  * The copy ctor creates a stand-alone element without neighbors
+	  * copying the data pointer and destroy method from @a src.
+	  * As the data is wrapped in a shared_ptr, data will not get
+	  * deleted unless the last reference is gone.
+	  *
+	  * @param[in] src reference to the element to copy.
+	**/
+	TSingleElement (const elem_t &src) noexcept
+	: base_t (src),
+	  data (src.data)
+	{ }
+
+
+	virtual ~TSingleElement() noexcept;
+
+
+	/* ===============================================
+	 * === Public methods                          ===
+	 * ===============================================
+	*/
+
+	/** @brief returns true if the data  was destroyed
+	  *
+	  * The destructor will try to get a final lock on the
+	  * element when it is destroyed. If another thread
+	  * acquires a lock between the data destruction and
+	  * this final dtor lock, destroy() will return "true".
+	  *
+	  * @return true if the element is within its destruction process.
+	**/
+	bool destroyed() const noexcept { return isDestroyed; }
+
+
+	/* ===============================================
+	 * === Public operators                        ===
+	 * ===============================================
+	*/
+
+	/** @brief assignment operator
+	  *
+	  * The assignment operator copies over the element and
+	  * the destroy method. This element will stay where it
+	  * is, and not change its position.
+	  *
+	  * @param[in] src const reference of the element to copy
+	  * @return reference to this element
+	**/
+	elem_t& operator= (const elem_t &src) noexcept
+	{
+		if (this != &src) {
+			PWX_LOCK_GUARD (elem_t, this)
+			data    = src.data;
+		}
+		return *this;
+	}
+
+
+	/** @brief dereferencing an element returns a reference to the stored data
+	  *
+	  * If the data pointer is nullptr, a pwx::CException with the name
+	  * "NullDataException" is thrown.
+	  *
+	  * @return a read/write reference to the stored data.
+	**/
+	data_t &operator*()
+	{
+		if (nullptr == data.get())
+			PWX_THROW ( "NullDataException",
+						"nullptr TSingleElement<T>->data",
+						"The pointer lhs->data to dereference is nullptr.")
+		return *data;
+	}
+
+
+	/** @brief dereferencing a constant element returns a constant reference to the stored data
+	  *
+	  * If the data pointer is nullptr, a pwx::CException with the name
+	  * "NullDataException" is thrown.
+	  *
+	  * @return a read only reference to the stored data.
+	**/
+	const data_t &operator*() const
+	{
+		if (nullptr == data.get())
+			PWX_THROW ( "NullDataException",
+						"nullptr TSingleElement<T>->data",
+						"The pointer lhs->data to dereference is nullptr.")
+		return *data;
+	}
+
+
+	/* ===============================================
+	 * === Public members                          ===
+	 * ===============================================
+	*/
+
+	share_t data;           //!< The data this list element points to, wrapped in a shared_ptr.
+	elem_t* next = nullptr; //!< The next element in the list or nullptr if this is the tail.
+
+
+private:
+
+	/* ===============================================
+	 * === Private members                         ===
+	 * ===============================================
+	*/
+
+	bool isDestroyed = false; //!< Set to false by the dtor to let other threads react if they ask destroyed().
 }; // struct TSingleElement
 
 
@@ -179,18 +230,18 @@ struct PWX_API TSingleElement : public CLockable
 **/
 template<typename data_t>
 TSingleElement<data_t>::~TSingleElement() noexcept
-  {
-    if (1 == data.use_count())
-      {
-        this->lock();
-        data.reset(); // the shared_ptr will delete the data now
-        this->unlock();
+{
+	isDestroyed = true;
 
-        // Do another locking, so that threads having had to wait while the data
-        // was destroyed have a chance now to react before the object is gone
-        PWX_LOCK_GUARD(elem_t, this)
-      }
-  }
+	if (1 == data.use_count()) {
+		PWX_LOCK_GUARD (elem_t, this)
+		PWX_TRY(data.reset()) // the shared_ptr will delete the data now
+		catch(...) { }
+	}
+	// Do another locking, so that threads having had to wait while the data
+	// was destroyed have a chance now to react before the object is gone.
+	PWX_LOCK_GUARD (elem_t, this)
+}
 
 } // namespace pwx
 
