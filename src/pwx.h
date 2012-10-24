a82c4e22aa7ac9502c48f9b1cc123ebbb1ae8b13
Sven Eden <sven.eden@gmx.de>
2012-10-24 08:53:40 +0200

    Version bump

--- src/pwx.h
+++ src/pwx.h
@@ -53,6 +53,7 @@
   * 0.8.6.8   2012-09-30  sed, PrydeWorX  Basic TSet added and tested. Mathematical methods follow.
   * 0.8.6.9   2012-10-02  sed, PrydeWorX  Added TVarDeleter that delegates given destroy methods for shared_ptr
   * 0.8.6.10  2012-10-08  sed, PrydeWorX  Added operator=/+/-/+=/-= to all containers and finished TSet implementation.
+  * 0.8.6.11  2012-10-24  sed, PrydeWorX  Many fixes, completed TSet and and set functions.
   @endverbatim
 **/
 
