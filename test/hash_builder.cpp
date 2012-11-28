c3dc4ccd60d5131dde5ac1036541d39eaf50eb5b
Sven Eden <sven.eden@gmx.de>
2012-11-28 11:04:01 +0100

    Completed the first version of the new hash_builder test program

--- test/hash_builder.cpp
+++ test/hash_builder.cpp
@@ -4,16 +4,110 @@ int32_t main(int32_t argc, char** argv)
 {
 	int32_t result = EXIT_SUCCESS;
 
-	if (argc != 2) {
-		cerr << "Usage:\n  " << argv[0] << " <destination directory>" << endl;
+	if (argc != 3) {
+		cerr << "Usage:\n  " << argv[0] << " <destination directory> <number of lines>" << endl;
 		return EXIT_FAILURE;
 	}
 
-	string destdir = argv[1];
+	std::string destdir = argv[1];
+	while (destdir.size() && (destdir[destdir.size() - 1] == '/'))
+		destdir.erase(destdir.size() - 1);
 
+	// check first argument
+	if (!destdir.size()) {
+		cerr << "No valid destination directory given!" << endl;
+		return EXIT_FAILURE;
+	} else if (!pwx_file_exists(destdir.c_str())) {
+		cerr << "output path \"" << destdir << "\" does not exist!" << endl;
+		return EXIT_FAILURE;
+	} else if (!pwx_file_isW(destdir.c_str())) {
+		cerr << "output path \"" << destdir << "\" is not writable!" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// check second argument
+	int32_t argMax = pwx::to_int32(argv[2]);
+	if (!(argMax > 0)) {
+		cerr << "Number \"" << argv[2] << "\" is no legal number (" << argMax << ")" << endl;
+		return EXIT_FAILURE;
+	}
+
+
+	int32_t maxInt16  = static_cast<int32_t>(std::numeric_limits<int16_t>::max())
+					  - static_cast<int32_t>(std::numeric_limits<int16_t>::lowest());
+	int32_t maxUInt16 = static_cast<int32_t>(std::numeric_limits<uint16_t>::max());
 
 
 	cout << "Building hash lists in \"" << destdir << "\"." << endl;
+
+	// --- int16_t ---
+	//-----------------
+	string destfile = destdir + "/hash_int16.csv";
+	result = build_hash_list_num<int16_t>(destfile, maxInt16 < argMax ? maxInt16 : argMax);
+
+	// --- uint16_t ---
+	//------------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_uint16.csv";
+		result = build_hash_list_num<uint16_t>(destfile, maxUInt16 < argMax ? maxUInt16 : argMax);
+	}
+
+	// --- int32_t ---
+	//-----------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_int32.csv";
+		result = build_hash_list_num<int32_t>(destfile, argMax);
+	}
+
+	// --- uint32_t ---
+	//------------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_uint32.csv";
+		result = build_hash_list_num<uint32_t>(destfile, argMax);
+	}
+
+	// --- int64_t ---
+	//-----------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_int64.csv";
+		result = build_hash_list_num<int64_t>(destfile, argMax);
+	}
+
+	// --- uint64_t ---
+	//------------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_uint64.csv";
+		result = build_hash_list_num<uint64_t>(destfile, argMax);
+	}
+
+	// --- float ---
+	//---------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_float.csv";
+		result = build_hash_list_num<float>(destfile, argMax);
+	}
+
+	// --- double ---
+	//----------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_double.csv";
+		result = build_hash_list_num<double>(destfile, argMax);
+	}
+
+	// --- long double ---
+	//---------------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_long_double.csv";
+		result = build_hash_list_num<long double>(destfile, argMax);
+	}
+
+	// --- char ---
+	//--------------
+	if (EXIT_SUCCESS == result) {
+		destfile = destdir + "/hash_string.csv";
+		result = build_hash_list_char(destfile, argMax);
+	}
+
 
 	return result;
 }
--- test/hash_builder.h
+++ test/hash_builder.h
@@ -8,8 +8,13 @@ using std::cout;
 using std::cerr;
 using std::endl;
 
+#include <limits>
 #include <string>
 using std::string;
+
+#include <unistd.h>
+#include <fstream>
+using std::ofstream;
 
 // The names are long, make them shorter:
 typedef std::chrono::high_resolution_clock hrClock;
@@ -18,6 +23,105 @@ using std::chrono::duration_cast;
 using std::chrono::milliseconds;
 
 #include <pwx.h>
+using pwx::RNG;
+
+/// @internal build a numerical hash list
+template<typename T>
+int32_t build_hash_list_num(string &outfile, int32_t cnt_)
+{
+	int32_t result = EXIT_SUCCESS;
+	T maxTval = std::numeric_limits<T>::max();
+	T minTval = std::numeric_limits<T>::lowest();
+	ofstream out(outfile.c_str());
+
+	if (!out.is_open() || !out.good()) {
+		cerr << "Error opening \"" << outfile << "\"" << endl;
+		return EXIT_FAILURE;
+	}
+
+	// Correct maxTval/minTval if this is no integer
+	if (isSameType(T, float)) {
+		maxTval = (T)5.0;
+		minTval = (T)-5.0;
+	} else if (isSameType(T, double)) {
+		maxTval = (T).5;
+		minTval = (T)-.5;
+	} else if (isSameType(T, long double)) {
+		maxTval = (T).05;
+		minTval = (T)-.05;
+	}
+
+	T randVal;
+
+	cout << "Writing " << cnt_ << " values into \"" << outfile << "\" ...";
+	cout.flush();
+
+	hrTime_t tStart = hrClock::now();
+
+	for (int32_t cnt = 0 ; out.good() && (cnt < cnt_); ++cnt) {
+		randVal = RNG.random(minTval, maxTval);
+		out << randVal << ";" << RNG.hash(randVal) << endl;
+	}
+
+	hrTime_t tEnd = hrClock::now();
+	auto tTimeNeeded = duration_cast<milliseconds>(tEnd - tStart).count();
+	cout << "(" << tTimeNeeded << " ms)";
+
+	if (!out.good())
+		result = EXIT_FAILURE;
+
+	if (out.is_open())
+		out.close();
+
+	if (EXIT_SUCCESS == result)
+		cout << " done" << endl;
+	else
+		cout << " ERROR!" << endl;
+
+	return result;
+}
+
+
+/// @internal build a C-String based hash list
+int32_t build_hash_list_char(string &outfile, int32_t cnt_)
+{
+	int32_t result = EXIT_SUCCESS;
+	ofstream out(outfile.c_str());
+
+	if (!out.is_open() || !out.good()) {
+		cerr << "Error opening \"" << outfile << "\"" << endl;
+		return EXIT_FAILURE;
+	}
+
+	char randVal[17];
+
+	cout << "Writing " << cnt_ << " values into \"" << outfile << "\" : ";
+	cout.flush();
+
+	hrTime_t tStart = hrClock::now();
+
+	for (int32_t cnt = 0 ; out.good() && (cnt < cnt_); ++cnt) {
+		RNG.random(randVal, 17, 17);
+		out << randVal << ";" << RNG.hash(randVal) << endl;
+	}
+
+	hrTime_t tEnd = hrClock::now();
+	auto tTimeNeeded = duration_cast<milliseconds>(tEnd - tStart).count();
+	cout << "(" << tTimeNeeded << " ms)";
+
+	if (!out.good())
+		result = EXIT_FAILURE;
+
+	if (out.is_open())
+		out.close();
+
+	if (EXIT_SUCCESS == result)
+		cout << " done" << endl;
+	else
+		cout << " ERROR!" << endl;
+
+	return result;
+}
 
 
 #endif // PWX_LIBPWX_TEST_HASH_BUILDER_H_INCLUDED
