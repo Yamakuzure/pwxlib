b032614c1c1728a4882395a47813a3b1fdd3b80e
Sven Eden <sven.eden@gmx.de>
2012-12-07 09:24:30 +0100

    changed the elements order in all tests to the new element storage order of TStack and TQueue

--- test/testStackQueue.h
+++ test/testStackQueue.h
@@ -63,13 +63,13 @@ int32_t testStackQueue (sEnv &env)
 	}
 
 	/***************************************************************************
-	** B) List elements, must be 5, 4, 3, 2, 1                                **
+	** B) List elements, must be 1, 2, 3, 4, 5                                **
 	***************************************************************************/
 	if (EXIT_SUCCESS == result) {
 		int32_t check[5] = { **intCont[0], **intCont[1], **intCont[2], **intCont[3], **intCont[4] };
-		cout << adjRight (4, 0) << ++env.testCount << " B) Elements are (5, 4, 3, 2, 1) : ";
+		cout << adjRight (4, 0) << ++env.testCount << " B) Elements are (1, 2, 3, 4, 5) : ";
 		cout << check[0] << ", " << check[1] << ", " << check[2] << ", " << check[3] << ", " << check[4];
-		if ( (5 == check[0]) && (4 == check[1]) && (3 == check[2]) && (2 == check[3]) && (1 == check[4])) {
+		if ( (1 == check[0]) && (2 == check[1]) && (3 == check[2]) && (4 == check[3]) && (5 == check[4])) {
 			cout << " - Success" << endl;
 			++env.testSuccess;
 		} else {
@@ -172,14 +172,14 @@ int32_t testStackQueue (sEnv &env)
 	}// End of test E
 
 	/***************************************************************************
-	** F) Check third container values (2, 4, 1, 3, 5)                        **
+	** F) Check third container values (5, 3, 1, 4, 2)                        **
 	***************************************************************************/
 	if (EXIT_SUCCESS == result) {
 		cout << adjRight (4, 0) << ++env.testCount << " F) Check third container" << endl;
-		cout << "         (2, 4, 1, 3, 5) - ";
+		cout << "         (5, 3, 1, 4, 2) - ";
 		int32_t check[5] = { **testContC[0], **testContC[1], **testContC[2], **testContC[3], **testContC[4] };
 		cout << check[0] << ", " << check[1] << ", " << check[2] << ", " << check[3] << ", " << check[4];
-		if ( (2 == check[0]) && (4 == check[1]) && (1 == check[2]) && (3 == check[3]) && (5 == check[4])) {
+		if ( (5 == check[0]) && (3 == check[1]) && (1 == check[2]) && (4 == check[3]) && (2 == check[4])) {
 			cout << " - Success" << endl;
 			++env.testSuccess;
 		} else {
@@ -191,14 +191,14 @@ int32_t testStackQueue (sEnv &env)
 
 
 	/***************************************************************************
-	** G) Substract first from the union (2, 4)                               **
+	** G) Substract first from the union (4, 2)                               **
 	***************************************************************************/
 	if (EXIT_SUCCESS == result) {
-		cout << adjRight (4, 0) << ++env.testCount << " G) Substract first container (2, 4) - ";
+		cout << adjRight (4, 0) << ++env.testCount << " G) Substract first container (4, 2) - ";
 		testContC -= testContA;
 		int32_t check[2] = { **testContC[0], **testContC[1] };
 		cout << check[0] << ", " << check[1];
-		if ( (2 == check[0]) && (4 == check[1])) {
+		if ( (4 == check[0]) && (2 == check[1])) {
 			cout << " - Success" << endl;
 			++env.testSuccess;
 		} else {
@@ -208,73 +208,6 @@ int32_t testStackQueue (sEnv &env)
 		}
 	} // End of test G
 
-/// @todo : Put into unified speed test once RNG is available for random access
-//  /** D) Speed test inserting, checking and removing MAXINTCOUNT integers **/
-//  if (EXIT_SUCCESS == result)
-//    {
-//      clock_t startTime = 0;
-//      clock_t endTime   = 0;
-//
-//      cout << adjRight(4,0) << ++env.testCount << " D) Speed test (" << MAXINTCOUNT << " elements) : "; cout.flush();
-//
-//      // for this test we need a container that does delete data:
-//      list_t spdCont; // The list without the do-not-destroy-destroy()-method
-//
-//      cout << "push"; cout.flush();
-//
-//      startTime = clock();
-//
-//      for (int32_t i = 1; i < (MAXINTCOUNT + 1); ++i)
-//        {
-//          // Note: As main() does a catch-all, no try{} needed here.
-//          int32_t* newInt = new int32_t(i);
-//          spdCont.push(newInt);
-//        }
-//
-//      if ((EXIT_SUCCESS == result) && (MAXINTCOUNT != static_cast<int32_t>(spdCont.size())))
-//        {
-//          cout << " FAILED: spdCont has " << spdCont.size() << " elements" << endl;
-//          result = EXIT_FAILURE;
-//        }
-//
-//      if (EXIT_SUCCESS == result)
-//        {
-//          cout << ", pop"; cout.flush();
-//
-//          for (int32_t i = 1; i < (MAXINTCOUNT + 1); ++i)
-//            {
-//              auto elem = spdCont.pop();
-//              if ( (isSameType(list_t, stack_t) && ((MAXINTCOUNT + 1 - i) != **elem) )
-//                || (i != **elem) )
-//                {
-//                  cout << "FAIL" << endl;
-//                  i = MAXINTCOUNT + 1;
-//                  result = EXIT_FAILURE;
-//                }
-//            }
-//        }
-//
-//      if (EXIT_SUCCESS == result)
-//        {
-//          if (spdCont.size())
-//            {
-//              cout << " FAILED: " << spdCont.size() << " items remain" << endl;
-//              result = EXIT_FAILURE;
-//            }
-//          else
-//            {
-//              endTime = clock();
-//              double msNeeded = 1000.0 * (static_cast<double>(endTime - startTime) / static_cast<double>(CLOCKS_PER_SEC));
-//              cout << adjRight(5,0) << msNeeded << " ms - Success" << endl;
-//            }
-//        }
-//
-//      if (EXIT_SUCCESS == result)
-//        ++env.testSuccess;
-//      else
-//        ++env.testFail;
-//
-//    } // End of sub test E
 	cout << endl;
 
 	return result;
