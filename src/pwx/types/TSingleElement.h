9984be9d762c6d2b52215725d21ac1f23ff8dbc2
Sven Eden <sven.eden@gmx.de>
2013-04-08 06:55:12 +0200

    TSingleElement: Added methods and rewrote others to handle the "neighborhood" of an element with as few locks as possible. Further added methods to optionally turn the thread safety measurements off.

--- src/pwx/types/TSingleElement.h
+++ src/pwx/types/TSingleElement.h
@@ -53,25 +53,60 @@ namespace pwx
   * The next element in the list can be retrieved using the public foo->next pointer.
   *
   * If you plan to use this type in a multi-threaded environment, you can use the
-  * getNext() and setNext() functions to manipulate the next pointer. These methods
-  * will lock the element prior any read/write access.
+  * getNext() and setNext() functions to manipulate the next pointer. See below for
+  * more on multi threaded usage.
   *
   * To insert any element into a list you can use insertNext() to have it inserted
-  * after the called element safely.
+  * after the called element safely. If you just added an element to a container
+  * you can use insert() to tell the element that it has been inserted into a
+  * container.
   *
-  * To remove an element from a list, you can use removeNext() to have it removed
-  * safely.
-  *
-  * However, as the next pointer is wrapped into std::atomic, you can use its
-  * load/store functions to avoid locking instead.
+  * To remove an element from a list, you can use removeNext() to have the element
+  * after this to be removed safely. If there is no other element to call removeNext()
+  * from, you can use an elements remove() function to tell it that it has been
+  * removed from its container. This will set the next pointer to nullptr as well.
   *
   * It is recommended that you use the much more advanced std::list unless you
   * need to store a very large number of elements and can not live with the
   * downside of every element having to be copied into the std::list.
   *
+  * <B>Notes on multi threaded environments</B>
+  *
   * If you plan to use an element in a strictly single-threaded way, you can use
   * the function do_locking(bool) inherited from CLockable to disable the locking
-  * mechanism.
+  * mechanism. You can then use insertNext() / removeNext() without the locking
+  * overhead. However, as the locking is enabled by default, it might be more
+  * convenient to simply use the next pointers directly.
+  *
+  * <I>Critical work flows</I>
+  *
+  * The following work flows can be troublesome if multiple threads perform
+  * concurrent tasks on an element:
+  * <TABLE border="1">
+  * <TR>
+  *   <TH>Task</TH><TH>Problematic action</TH><TH>Solution</TH>
+  * </TR><TR>
+  *   <TD>Retrieve next element</TD><TD>Remove this element</TD>
+  *   <TD>Elements know when they are removed. getNext() then delivers the
+  * previously stored pointer, if any.</TD>
+  * </TR><TR>
+  *   <TD>Retrieve next element</TD><TD>Move element to different container</TD>
+  *   <TD>This is not detectable, so never move an element. Remove and copy insert it!</TD>
+  * </TR><TR>
+  *   <TD>Insert an element after this</TD><TD>Either element destroyed by another thread</TD>
+  *   <TD>insertNext() will lock both this and the new next element. Further it checks
+  * whether any is destroyed and only inserts the element if both are not marked as destroyed.<BR />
+  * If either is marked as destroyed, a pwx::CException is thrown, as those conditions imply
+  * serious bugs.</TD>
+  * </TR><TR>
+  *   <TD>Remove the next element</TD><TD>The next element gets removed or another element is
+  * inserted between the two elements by another thread</TD>
+  *   <TD>removeNext() will try to lock both elements after one another in a Release->Yield->Lock
+  * cycle until both are locked or the next element changes. In the latter event the method does
+  * not remove the element, as it is gone.<BR />
+  * If the next element goes away before it can be removed, a pwx::CException is thrown.</TD>
+  * </TR>
+  * </TABLE>
 **/
 template<typename data_t>
 struct PWX_API TSingleElement : public VElement
@@ -82,10 +117,10 @@ struct PWX_API TSingleElement : public VElement
 	 * ===============================================
 	*/
 
-	typedef VElement                base_t;
-	typedef TSingleElement<data_t>  elem_t;
-	typedef std::shared_ptr<data_t> share_t;
-	typedef std::atomic<elem_t*>    neighbor_t;
+	typedef VElement                base_t;     //!< Base type of this element
+	typedef TSingleElement<data_t>  elem_t;     //!< Type of this element
+	typedef std::shared_ptr<data_t> share_t;    //!< data_t wrapped in std::shared_ptr
+	typedef std::atomic<elem_t*>    neighbor_t; //!< elem_t* wrapped in std::atomic
 
 
 	/* ===============================================
@@ -101,8 +136,7 @@ struct PWX_API TSingleElement : public VElement
 	  * @param[in] destroy_ A pointer to a function that is to be used to destroy the data
 	**/
 	TSingleElement (data_t* data_, void (*destroy_) (data_t* data_)) noexcept
-	: data (data_, TVarDeleter<data_t> (destroy_)),
-	  next(ATOMIC_VAR_INIT(nullptr))
+	: data (data_, TVarDeleter<data_t> (destroy_))
 	{ }
 
 
@@ -127,6 +161,9 @@ struct PWX_API TSingleElement : public VElement
 	  * copying the data pointer and destroy method from @a src.
 	  * As the data is wrapped in a shared_ptr, data will not get
 	  * deleted unless the last reference is gone.
+	  *
+	  * <B>Important</B>: Whether the element does locking or not
+	  * is *not* copied. It will silently turned on by default!
 	  *
 	  * @param[in] src reference to the element to copy.
 	**/
@@ -153,7 +190,22 @@ struct PWX_API TSingleElement : public VElement
 	**/
 	elem_t* getNext() const noexcept
 	{
+		if ( isRemoved.load(std::memory_order_acquire) )
+			return oldNext.load(std::memory_order_acquire);
 		return next.load(std::memory_order_acquire);
+	}
+
+
+	/** @brief tell the element that it has been inserted.
+	  *
+	  * If an element becomes the last or only element of a container,
+	  * it has no next element. This method can be used to tell the
+	  * element that it is stored in a container even without a next
+	  * pointer set.
+	**/
+	void insert() const noexcept
+	{
+		isRemoved.store(false, std::memory_order_release);
 	}
 
 
@@ -163,26 +215,68 @@ struct PWX_API TSingleElement : public VElement
 	  * of this element, but the next pointer of the inserted element
 	  * safely, too, in a multi-threaded environment.
 	  *
+	  * If either this or the new element is marked as destroyed,
+	  * a pwx::CException is thrown. Such a condition implies that
+	  * there is something seriously wrong.
+	  *
+	  * If this element is marked as removed, it is simply assumed to
+	  * be a single element in a container and will be marked as not
+	  * removed by the insert.
+	  *
+	  * On the other hand, if @a new_next is either this element or
+	  * nullptr, the method simply does nothing.
+	  *
 	  * @param[in] new_next target where the next pointer should point at.
 	**/
-	void insertNext(elem_t* new_next) noexcept
+	void insertNext(elem_t* new_next)
 	{
-		if (!destroyed() && new_next && (new_next != this) && !new_next->destroyed()) {
+		if (!new_next || (new_next == this))
+			return;
+
+		if (!destroyed() && !new_next->destroyed()) {
 			PWX_DOUBLE_LOCK(elem_t, this, elem_t, new_next)
 
-			// Now that we have the double lock, it is crucial to
-			// check again. Otherwise we might just insert a destroyed element.
-			if (!destroyed() && !new_next->destroyed()) {
-				// Safe old next pointer.
-				elem_t* oldNext = this->getNext();
+			/* Now that we have the double lock, it is crucial to
+			 * check again. Otherwise we might just insert a destroyed element.
+			*/
+			if (destroyed())
+				PWX_THROW("Illegal_Insert", "Destroyed elements can't insert",
+						"The inserting element has been destroyed while waiting for the lock!")
 
-				// Set the neighborhood of the new next
-				new_next->next.store(oldNext, std::memory_order_relaxed);
+			if (new_next->destroyed())
+				PWX_THROW("Illegal_Insert", "Can't insert a destroyed element",
+						"The element to insert has been destroyed while waiting for the lock!")
 
-				// Store new next neighbor.
-				setNext(new_next);
-			}
-		}
+			// Safe old next pointer.
+			elem_t* xOldNext = this->getNext();
+
+			// Set the neighborhood of the new next
+			new_next->next.store(xOldNext, std::memory_order_relaxed);
+
+			// Store new next neighbor and tell it that it has been inserted
+			setNext(new_next);
+			new_next->insert();
+		} else if (destroyed())
+			PWX_THROW("Illegal_Insert", "Destroyed elements can't insert",
+					"Tried to insert an element after an already destroyed element!")
+		else
+			PWX_THROW("Illegal_Insert", "Can't insert a destroyed element",
+					"Tried to insert an element that has already been destroyed!")
+	}
+
+
+	/** @brief tell the element that it has been removed.
+	  *
+	  * Whenever you remove an element from a container you should
+	  * call this method to tell it that it has been removed.
+	  * The next pointer of the element will be set to nullptr
+	  * by this method.
+	**/
+	void remove() const noexcept
+	{
+		PWX_LOCK_GUARD(elem_t, const_cast<elem_t*>(this))
+		setNext(nullptr);
+		isRemoved.store(true, std::memory_order_release);
 	}
 
 
@@ -190,18 +284,21 @@ struct PWX_API TSingleElement : public VElement
 	  *
 	  * This method removes the successor of this element
 	  * from a list in a thread safe way.
+	  *
+	  * If the next element gets moved or removed while this
+	  * thread waits for the lock, a pwx::CException is thrown.
 	**/
-	void removeNext() noexcept
+	void removeNext()
 	{
 		elem_t* toRemove = nullptr;
 		// Do an acquiring test before the element is actually locked
 		if ( (toRemove = this->getNext()) ) {
-			PWX_LOCK(toRemove)
-			elem_t* oldNext = nullptr;
+			PWX_LOCK_GUARD(elem_t, toRemove)
+			elem_t* xOldNext = toRemove->getNext();
 
 			/* See notes in TDoubleElement::remove() */
 
-			// 1: Handle previous neighbor aka "this" first
+			// Do a release->yield->lock cycle until this is locked
 			while ((this->getNext() == toRemove) && !this->try_lock()) {
 				// Can't lock this, so yield until possible
 				PWX_UNLOCK(toRemove)
@@ -211,28 +308,32 @@ struct PWX_API TSingleElement : public VElement
 
 			// Now if next still points to toRemove, this is locked:
 			if (this->getNext() == toRemove) {
-				this->setNext(toRemove->getNext());
+				this->setNext(xOldNext);
 				this->unlock();
-			}
+			} else
+				// Otherwise toRemove went away, which is bad
+				PWX_THROW("Illegal_Remove", "Next element to remove went away",
+						"A next element to remove went away while waiting for the lock!")
 
-			// 2: Clear toRemove neighbor
-			toRemove->setNExt(nullptr);
-
-			PWX_UNLOCK(toRemove)
-		} // End of having at least one neighbor to handle
+			// 2: Remove neighborhood:
+			toRemove->remove();
+		} // End of having a neighbor to handle
 	}
 
 
 	/** @brief set the next pointer to another element.
 	  *
-	  * This method uses atomic::storer() and is therefore safe to use
+	  * This method uses atomic::store() and is therefore safe to use
 	  * in a multi-threaded environment.
 	  *
 	  * @param[in] new_next target where the next pointer should point at.
 	**/
 	void setNext(elem_t* new_next) noexcept
 	{
+		elem_t* currNext = next.load(std::memory_order_acquire);
 		next.store(new_next, std::memory_order_release);
+		if (currNext)
+			oldNext.store(currNext, std::memory_order_release);
 	}
 
 
@@ -303,8 +404,8 @@ struct PWX_API TSingleElement : public VElement
 	 * ===============================================
 	*/
 
-	share_t    data; //!< The data this list element points to, wrapped in a shared_ptr.
-	neighbor_t next; //!< The next element in the list or nullptr if this is the tail.
+	share_t    data; 							//!< The data this list element points to, wrapped in a shared_ptr.
+	neighbor_t next = ATOMIC_VAR_INIT(nullptr); //!< The next element in the list or nullptr if this is the tail.
 
 
 protected:
@@ -315,6 +416,16 @@ protected:
 	 */
 
 	using base_t::isDestroyed;
+	using base_t::isRemoved;
+
+private:
+
+	/* ===============================================
+	 * === Private members                         ===
+	 * ===============================================
+	 */
+
+	neighbor_t oldNext = ATOMIC_VAR_INIT(nullptr);
 
 }; // struct TSingleElement
 
