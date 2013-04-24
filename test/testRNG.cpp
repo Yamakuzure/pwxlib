26079f8b1a8a9c0e491df9304bccaf29f8cf4856
Sven Eden <yamakuzure@gmx.net>
2013-04-24 17:40:05 +0200

    Added testing of the RNG.noise() functions

--- test/testRNG.cpp
+++ test/testRNG.cpp
@@ -1,5 +1,11 @@
 #include "testRNG_func.h"
 
+#include <limits>
+#include <cmath>
+
+const double fullMaxDouble = std::numeric_limits<double>::max();
+const double fullMinDouble = std::numeric_limits<double>::lowest();
+
 /** @brief central function to test pwx::RNG
   *
   *************************************************************************
@@ -13,12 +19,13 @@
 **/
 int32_t testRNG (sEnv &env)
 {
-	int32_t result = EXIT_SUCCESS;
+	int32_t result           = EXIT_SUCCESS;
+	uint32_t testMaxElements = env.doSpeed ? rngMaxElements : 10;
 
-	cout << "Test CRandom instance pwx::RNG\n------------------------------" << endl;
+	cout << "\nTest CRandom instance pwx::RNG\n------------------------------" << endl;
 
 	/************************************************************************
-	** C) Random number functions                                          **
+	** A) Random number functions                                          **
 	************************************************************************/
 	cout << " A) Random functions (max) / (min, max)" << endl;
 
@@ -88,7 +95,7 @@ int32_t testRNG (sEnv &env)
 	/************************************************************************
 	** B) Hash functions                                                   **
 	************************************************************************/
-	cout << " B) Hash functions with " << (env.doSpeed ? hashMaxElements : 10);
+	cout << " B) Hash functions with " << testMaxElements;
 	cout << " keys (min/max (hash / loop ms))" << endl;
 	cout << adjRight (4, 0) << ++env.testCount << " int16_t    : "; cout.flush();
 	testRNG_hash<int16_t>(env);
@@ -116,6 +123,131 @@ int32_t testRNG (sEnv &env)
 	/************************************************************************
 	** C) Noise functions                                                  **
 	************************************************************************/
+	cout << " C) Noise functions with " << testMaxElements << " keys (min/max)" << endl;
+	// This is so small that we can do it here:
+	int32_t maxX = testMaxElements;
+	int32_t maxY = static_cast<int32_t>(std::floor(std::pow(static_cast<float>(maxX), 1. / 2.)));
+	int32_t maxZ = static_cast<int32_t>(std::floor(std::pow(static_cast<float>(maxX), 1. / 3.)));
+	int32_t maxW = static_cast<int32_t>(std::floor(std::pow(static_cast<float>(maxX), 1. / 4.)));
+	int32_t miss = 0; // to correct rounding errors
+	if (maxX < 1) maxX = 1;
+	if (maxY < 1) maxY = 1;
+	if (maxZ < 1) maxZ = 1;
+	if (maxW < 1) maxW = 1;
+
+	DEBUG_LOG("noise", "maxX: %d, maxY: %d, maxZ: %d, maxW: %d", maxX, maxY, maxZ, maxW)
+
+	double curNoise = 0.;
+
+	// A) 1 dimension
+	double maxNoise = fullMinDouble;
+	double minNoise = fullMaxDouble;
+	DEBUG_LOG("noise", "%d + %d missed", maxX, miss)
+	cout << adjRight (4, 0) << ++env.testCount << " 1 dimension  x       : "; cout.flush();
+	for (int32_t x = 0; x < maxX; ++x) {
+		curNoise = RNG.noise(x);
+		if (curNoise > maxNoise) maxNoise = curNoise;
+		if (curNoise < minNoise) minNoise = curNoise;
+	}
+	cout << adjRight(2,7) << minNoise << " / " << adjRight(2,7) << maxNoise;
+	if (maxNoise > minNoise) {
+		cout << " : SUCCESS" << endl;
+		++env.testSuccess;
+	} else {
+		cout << " : FAILED!" << endl;
+		++env.testFail;
+	}
+
+	// B) 2 dimensions
+	maxNoise = fullMinDouble;
+	minNoise = fullMaxDouble;
+	miss     = testMaxElements - static_cast<int32_t>(std::pow(maxY, 2.));
+	DEBUG_LOG("noise", "%d + %d missed", static_cast<int32_t>(std::pow(maxY, 2.)), miss)
+	cout << adjRight (4, 0) << ++env.testCount << " 2 dimensions x,y     : "; cout.flush();
+	for (int32_t x = 0; x < maxY; ++x) {
+		for (int32_t y = 0; y < maxY; ++y) {
+			curNoise = RNG.noise(x,y);
+			if (curNoise > maxNoise) maxNoise = curNoise;
+			if (curNoise < minNoise) minNoise = curNoise;
+		}
+	}
+	// Add "miss"ed iterations
+	for (int32_t val = 0; val < miss; ++val) {
+		curNoise = RNG.noise(maxY + val, maxY + val);
+		if (curNoise > maxNoise) maxNoise = curNoise;
+		if (curNoise < minNoise) minNoise = curNoise;
+	}
+	cout << adjRight(2,7) << minNoise << " / " << adjRight(2,7) << maxNoise;
+	if (maxNoise > minNoise) {
+		cout << " : SUCCESS" << endl;
+		++env.testSuccess;
+	} else {
+		cout << " : FAILED!" << endl;
+		++env.testFail;
+	}
+
+	// C) 3 dimensions
+	maxNoise = fullMinDouble;
+	minNoise = fullMaxDouble;
+	miss     = testMaxElements - static_cast<int32_t>(std::pow(maxZ, 3.));
+	DEBUG_LOG("noise", "%d + %d missed", static_cast<int32_t>(std::pow(maxZ, 3.)), miss)
+	cout << adjRight (4, 0) << ++env.testCount << " 3 dimensions x,y,z   : "; cout.flush();
+	for (int32_t x = 0; x < maxZ; ++x) {
+		for (int32_t y = 0; y < maxZ; ++y) {
+			for (int32_t z = 0; z < maxZ; ++z) {
+				curNoise = RNG.noise(x,y,z);
+				if (curNoise > maxNoise) maxNoise = curNoise;
+				if (curNoise < minNoise) minNoise = curNoise;
+			}
+		}
+	}
+	// Add "miss"ed iterations
+	for (int32_t val = 0; val < miss; ++val) {
+		curNoise = RNG.noise(maxZ + val, maxZ + val, maxZ + val);
+		if (curNoise > maxNoise) maxNoise = curNoise;
+		if (curNoise < minNoise) minNoise = curNoise;
+	}
+	cout << adjRight(2,7) << minNoise << " / " << adjRight(2,7) << maxNoise;
+	if (maxNoise > minNoise) {
+		cout << " : SUCCESS" << endl;
+		++env.testSuccess;
+	} else {
+		cout << " : FAILED!" << endl;
+		++env.testFail;
+	}
+
+	// D) 4 dimensions
+	maxNoise = fullMinDouble;
+	minNoise = fullMaxDouble;
+	miss     = testMaxElements - static_cast<int32_t>(std::pow(maxW, 4.));
+	DEBUG_LOG("noise", "%d + %d missed", static_cast<int32_t>(std::pow(maxW, 4.)), miss)
+	cout << adjRight (4, 0) << ++env.testCount << " 4 dimensions x,y,z,w : "; cout.flush();
+	for (int32_t x = 0; x < maxW; ++x) {
+		for (int32_t y = 0; y < maxW; ++y) {
+			for (int32_t z = 0; z < maxW; ++z) {
+				for (int32_t w = 0; w < maxW; ++w) {
+					curNoise = RNG.noise(x,y,z,w);
+					if (curNoise > maxNoise) maxNoise = curNoise;
+					if (curNoise < minNoise) minNoise = curNoise;
+				}
+			}
+		}
+	}
+	// Add "miss"ed iterations
+	for (int32_t val = 0; val < miss; ++val) {
+		curNoise = RNG.noise(maxW + val, maxW + val, maxW + val, maxW + val);
+		if (curNoise > maxNoise) maxNoise = curNoise;
+		if (curNoise < minNoise) minNoise = curNoise;
+	}
+	cout << adjRight(2,7) << minNoise << " / " << adjRight(2,7) << maxNoise;
+	if (maxNoise > minNoise) {
+		cout << " : SUCCESS" << endl;
+		++env.testSuccess;
+	} else {
+		cout << " : FAILED!" << endl;
+		++env.testFail;
+	}
+
 
 	/************************************************************************
 	** D) Random name functions                                            **
