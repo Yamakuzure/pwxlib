b12b8e56ae261fa2bbfe5896b99f2fb5f8d369d4
Sven Eden <sven.eden@prydeworx.com>
2019-03-13 07:51:12 +0100

    doxygen: enhance documentation generation
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- meson/find_source.sh
+++ meson/find_source.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-find "$MESON_SOURCE_ROOT"/src/ -type f -name '*.h' -or -name '*.cpp' | sort -u
+
+# Wrappers are provided from the call
+for x in $@ ; do
+        echo "$x" | sed -e "s,$MESON_SOURCE_ROOT,..,g"
+done
+
+# Headers must be found and go first
+find "$MESON_SOURCE_ROOT"/src/ -type f -name '*.h' | \
+        sort -u | \
+        sed -e "s,$MESON_SOURCE_ROOT,..,g"
+
+# Followed by the source files
+find "$MESON_SOURCE_ROOT"/src/ -type f -name '*.cpp' | \
+        sort -u | \
+        sed -e "s,$MESON_SOURCE_ROOT,..,g"
