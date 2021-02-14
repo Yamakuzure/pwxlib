ed136ddb2cb82915d49fb23089e4949a567e75b9
Sven Eden <sven.eden@prydeworx.com>
2021-02-14 14:59:34 +0100

    basic/pwx_trace_info.h: Add missing documentation bits.
    Signed-off-by: Sven Eden <sven.eden@prydeworx.com>

--- src/basic/pwx_trace_info.h
+++ src/basic/pwx_trace_info.h
@@ -2,13 +2,45 @@
 #define PWX_PWXLIB_SRC_BASIC_PWX_TRACE_INFO_H_INCLUDED 1
 #pragma once
 
+/** @file pwx_trace_info.h
+  *
+  * @brief Helper functions to get tracing information strings out of location data
+  *
+  * (c)  2007 - 2021 PrydeWorX
+  * @author Sven Eden, PrydeWorX - Adendorf, Germany
+  *         sven.eden@prydeworx.com
+  *         https://github.com/Yamakuzure/pwxlib ; https://pwxlib.prydeworx.com
+  *
+  * The PrydeWorX Library is free software under MIT License
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  * History and change log are maintained in pwxlib.h
+**/
+
+
+#include "basic/pwx_compiler.h"
+#include "basic/pwx_macros.h"
 
 #include <cstddef>
 #include <cstdint>
 #include <cstdlib>
-
-#include "basic/pwx_compiler.h"
-#include "basic/pwx_macros.h"
 
 
 /** @def PWX_TRACE_INFO
